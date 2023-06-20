c962d703dc759e07f4a1484e43962d0b18e33e8e
Francisco Pedroza <francisco.pedroza@fundacion-jala.org>
2023-06-19 19:39:58 -0600

    Tarea 2 corregida

--- game/components/enemies/spider.py
+++ game/components/enemies/spider.py
@@ -7,20 +7,16 @@ from game.utils.constants import SCREEN_WIDTH, SCREEN_HEIGHT, LEFT, RIGHT
 class Spider(Enemy):
     WIDTH = 40
     HEIGHT = 60
+    SPEED_Y = 3
+    SPEED_X = 7
+    INTERVAL = 15
+    
 
     def __init__(self):
         self.image = ENEMY_2
         self.image = pygame.transform.scale(self.image, (self.WIDTH, self.HEIGHT))
         super().__init__(self.image)
-        self.SPEED_Y = 3
-        self.SPEED_X = 7
-        self.INTERVAL = 15
-        self.is_alive = True
-
-    def update(self):
-        if self.rect.y >= SCREEN_HEIGHT:
-            self.is_alive = False
-        self.move()
+    
 
     def move(self):
         self.rect.y += self.SPEED_Y
